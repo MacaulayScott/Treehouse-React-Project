de1b44e489484d90f0a6e3545c088fa9912def81
Macaulay Scott <macaulay.scott@averment.digital>
2020-12-10 13:35:48 +0000

    addition of a few components

---
+++ src/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './header.css' 
+
+const Header = () => (
+    <header>
+      <h1 className="score">Scoreboard</h1>
+      <span className="stats">Player : 1</span>
+    </header>
+  );
+
+
+
+export default Header;
